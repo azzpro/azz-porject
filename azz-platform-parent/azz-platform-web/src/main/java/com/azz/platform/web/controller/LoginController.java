a609e2018f535c131c42ac400d21a69c88d39788
jianlin <m18676389907@163.com>
2018-10-16 13:55:29 +0800

    1

--- azz-platform-parent/azz-platform-web/src/main/java/com/azz/platform/web/controller/LoginController.java
+++ azz-platform-parent/azz-platform-web/src/main/java/com/azz/platform/web/controller/LoginController.java
@@ -4,9 +4,11 @@
  * Copyright (c) 2018. 爱智造.
  * 注意：本内容仅限于爱智造内部传阅，禁止外泄以及用于其他的商业目的
  ******************************************************************************/
- 
+
 package com.azz.platform.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,31 +19,40 @@ import com.azz.login.api.LoginService;
 import com.azz.login.pojo.Login;
 
 /**
- * <P>TODO</P>
+ * <P>
+ * TODO
+ * </P>
+ * 
  * @version 1.0
- * @author 刘建麟  2018年10月15日 下午3:05:46
+ * @author 刘建麟 2018年10月15日 下午3:05:46
  */
 @RestController
-@RequestMapping("/api/login")
+@RequestMapping("/azz/api/login")
 public class LoginController {
-	
-	@Autowired
-	private LoginService lsc;
-	
-	@RequestMapping(value="goLogin",produces = "application/json;charset=UTF-8")
-	public JSONObject goLogin(String data) {
-		JSONObject object = JSON.parseObject(data);
-		JSONObject s = (JSONObject)object.get("field");
+
+	private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
+
+	/**
+	 * <p>
+	 * 登录
+	 * </p>
+	 * 
+	 * @param name
+	 * @param password
+	 * @return
+	 * @author 刘建麟 2018年10月16日 上午10:49:10
+	 */
+	@RequestMapping("goLogin")
+	public JSONObject goLogin(String name, String password) {
+		LOG.info("用户名:" + name + ":密码:" + password);
 		JSONObject jb = new JSONObject();
 		JSONObject jb1 = new JSONObject();
-		Login login = lsc.getLogin(s.getString("username"), s.getString("password"));
-		if(null != login) {
-			jb1.put("access_token", "aaaaaaaa");
-			jb.put("code", 0);
-			jb.put("msg", "登入成功");
-			jb.put("data", jb1);
-		}
+		//token
+		jb1.put("access_token", "aaaaaaaa");
+		jb.put("code", 0);
+		jb.put("msg", "登入成功");
+		jb.put("data", jb1);
 		return jb;
 	}
-}
 
+}

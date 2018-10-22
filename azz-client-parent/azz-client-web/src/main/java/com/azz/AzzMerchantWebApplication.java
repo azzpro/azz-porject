0b24470ba0ba0b513c7a279a5930d5115bc38356
jianlin <m18676389907@163.com>
2018-10-22 09:53:50 +0800

    client project

---
+++ azz-client-parent/azz-client-web/src/main/java/com/azz/AzzMerchantWebApplication.java
@@ -0,0 +1,29 @@
+package com.azz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableEurekaClient
+@SpringBootApplication
+@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class,DataSourceTransactionManagerAutoConfiguration.class})
+@EnableFeignClients
+public class AzzMerchantWebApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+	System.setProperty("spring.devtools.restart.enabled", "true");
+	SpringApplication.run(AzzMerchantWebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	return builder.sources(this.getClass());
+    }
+
+}

6338562af817b04cb2b611312c991da5882b4ea3
jianlin <m18676389907@163.com>
2018-10-22 16:47:51 +0800

    1

--- azz-client-parent/azz-client-web/src/main/java/com/azz/shiro/CustomRealm.java
+++ azz-client-parent/azz-client-web/src/main/java/com/azz/shiro/CustomRealm.java
@@ -1,28 +1,25 @@
 /*******************************************************************************
  * Project Key : CPPII
- * Create on 2018年10月16日 上午9:52:54
+ * Create on 2018年10月16日 上午9:51:10
  * Copyright (c) 2018. 爱智造.
  * 注意：本内容仅限于爱智造内部传阅，禁止外泄以及用于其他的商业目的
  ******************************************************************************/
 
 package com.azz.shiro;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
-import org.apache.shiro.authc.SimpleAuthenticationInfo;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.authz.AuthorizationInfo;
-import org.apache.shiro.authz.SimpleAuthorizationInfo;
-import org.apache.shiro.realm.AuthorizingRealm;
-import org.apache.shiro.subject.PrincipalCollection;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
-import com.azz.core.common.JsonResult;
-import com.azz.core.common.errorcode.SystemErrorCode;
-import com.azz.core.exception.SuppressedException;
-import com.azz.platform.user.api.UserService;
-import com.azz.platform.user.pojo.bo.LoginParam;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * <P>
@@ -30,55 +27,77 @@ import com.azz.platform.user.pojo.bo.LoginParam;
  * </P>
  * 
  * @version 1.0
- * @author 黄智聪 2018年10月16日 上午9:52:54
+ * @author 黄智聪 2018年10月16日 上午9:51:10
  */
-public class CustomRealm extends AuthorizingRealm {
+@Configuration
+public class ShiroConfig {
+    @Bean
+    public ShiroFilterFactoryBean shirFilter(SecurityManager securityManager) {
+	ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+	// 必须设置 SecurityManager
+	shiroFilterFactoryBean.setSecurityManager(securityManager);
+	// setLoginUrl 如果不设置值，默认会自动寻找Web工程根目录下的"/login.jsp"页面 或 "/login" 映射
+	shiroFilterFactoryBean.setLoginUrl("/azz/api/user/noLogin");
+	// 设置无权限时跳转的 url;
+	shiroFilterFactoryBean.setUnauthorizedUrl("/azz/api/user/noPermission");
+	// 设置拦截器
+	Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+	// 开放登陆接口
+	filterChainDefinitionMap.put("/azz/api/user/login", "anon");
+	// 其余接口一律拦截
+	// 主要这行代码必须放在所有权限设置的最后，不然会导致所有 url 都被拦截
+	filterChainDefinitionMap.put("/**", "authc");
+	shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+	return shiroFilterFactoryBean;
+    }
 
-    @Autowired
-    UserService userService;
-
-    /**
-     * 获取身份验证信息 Shiro中，最终是通过 Realm 来获取应用程序中的用户、角色及权限信息的。
-     *
-     * @param authenticationToken
-     *            用户身份信息 token
-     * @return 返回封装了用户信息的 AuthenticationInfo 实例
-     */
-    @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken)
-	    throws AuthenticationException {
-	UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
-	String phoneNumber = token.getUsername();
-	String password = new String((char[]) token.getCredentials());
-	LoginParam param = new LoginParam(phoneNumber, password);
-	JsonResult<String> jr = userService.loginAuth(param);
-	if(jr.getCode() != SystemErrorCode.SUCCESS.getCode()) {
-	    AuthenticationException authException = new AuthenticationException();
-	    authException.addSuppressed(new SuppressedException(jr.getCode(),jr.getMsg()));
-	    throw authException;
-	}
-	SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(token.getPrincipal(), password, getName());
-	return info;
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+	// 对响应头进行CORS授权
+	ShiroCorsRegistration corsRegistration = new ShiroCorsRegistration("*");
+	corsRegistration
+		// 允许向该服务器提交请求的URI，*表示全部允许
+		.allowedOrigins("*")
+		// 允许提交请求的方法，*表示全部允许
+		.allowedMethods("*")
+		// 允许的头信息,*标识全部允许
+		.allowedHeaders("*")
+		// 暴露的头信息
+		.exposedHeaders(HttpHeaders.SET_COOKIE)
+		// 允许Cookie跨域，在做登录校验的时候有用
+		.allowCredentials(true);
+	// 注册CORS过滤器
+	UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+	// 第一个参数表示过滤的url,*表示过滤所有
+	configurationSource.registerCorsConfiguration("/**", corsRegistration.getCorsConfiguration());
+	CorsFilter corsFilter = new CorsFilter(configurationSource);
+	return new FilterRegistrationBean(corsFilter);
     }
 
     /**
-     * 获取授权信息
-     *
-     * @param principalCollection
-     * @return
+     * 注入 securityManager
      */
-    @Override
-    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
-	/*System.out.println("————权限认证————");
-	String username = (String) SecurityUtils.getSubject().getPrincipal();*/
-	SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-	/*// 获得该用户角色
-	String role = null;
-	Set<String> set = new HashSet<>();
-	// 需要将 role 封装到 Set 作为 info.setRoles() 的参数
-	set.add(role);
-	// 设置该用户拥有的角色
-	info.setRoles(set);*/
-	return info;
+    @Bean
+    public SecurityManager securityManager() {
+	DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+	// 设置realm.
+	securityManager.setRealm(customRealm());
+	securityManager.setSessionManager(sessionManager());
+	return securityManager;
+    }
+    
+    @Bean
+    public SessionManager sessionManager() {
+	return new ShiroSessionManager();
+    }
+    
+    /**
+     * 自定义身份认证 realm;
+     * <p>
+     * 必须写这个类，并加上 @Bean 注解，目的是注入 CustomRealm， 否则会影响 CustomRealm类 中其他类的依赖注入
+     */
+    @Bean
+    public CustomRealm customRealm() {
+	return new CustomRealm();
     }
 }

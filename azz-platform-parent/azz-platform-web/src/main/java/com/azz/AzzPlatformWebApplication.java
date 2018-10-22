dda8b1942f1dba13ff436120c127e06a91ea7150
jianlin <m18676389907@163.com>
2018-10-22 16:56:21 +0800

    1

--- azz-platform-parent/azz-platform-web/src/main/java/com/azz/AzzPlatformWebApplication.java
+++ azz-platform-parent/azz-platform-web/src/main/java/com/azz/AzzPlatformWebApplication.java
@@ -1,7 +1,10 @@
 package com.azz;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,6 +12,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableAutoConfiguration(exclude= {DataSourceAutoConfiguration.class})
 @EnableFeignClients
 public class AzzPlatformWebApplication extends SpringBootServletInitializer {
 

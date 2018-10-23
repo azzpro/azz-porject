05fdd6a04ad5f630ea4d353da41aa06848e56274
jianlin <m18676389907@163.com>
2018-10-23 17:00:45 +0800

    工具类

--- azz-common-parent/azz-common-core/src/main/java/com/azz/core/constants/FileConstants.java
+++ azz-common-parent/azz-common-core/src/main/java/com/azz/core/constants/FileConstants.java
@@ -13,12 +13,58 @@ package com.azz.core.constants;
  */
 public class FileConstants {
 	
+	//图片 bucketname 
+	public static final String IMAGE_BUCKETNAME = "azz-image";
+	
+	//文件bucketname
+	public static final String FILE_BUCKETNAME = "azz-file";
+	
+	//平台端图片文件夹
+	public static final String PLATFORM_IMAGE_PATH = "plat-image";
+	
+	//商户端图片文件夹
+	public static final String MERCHANT_IMAGE_PATH = "merchant-image";
+		
+	//客户端端图片文件夹
+	public static final String CLIENT_IMAGE_PATH = "client-image";
+	
+	//法人图片
 	public static final String AZZ_LEGAL_PERSON_PATH = "legal_person_pic";
 	
+	//营业执照
 	public static final String AZZ_BUSINESS_LICENSE_PATH = "business_license_pic";
 	
-	public static final int AZZ_IMAGE_TYPE = 1;
+	//图像
+	public static final String AZZ_AVATAR_PATH = "avatar_pic";
 	
-	public static final String AZZ_IMAGE_SUFFIX = "bmp|jpg|png";
+	//其他图片
+	public static final String AZZ_OTHER_PATH = "other_pic";
+	
+	//平台端
+	public static final int AZZ_PLATFORM = 1;
+	
+	//商户端
+	public static final int AZZ_MERCHANT = 2;
+	
+	//客户端
+	public static final int AZZ_CLIENT = 3;
+	
+	//法人图片
+	public static final int AZZ_LEGAL_IMAGE_TYPE = 1;
+	
+	//营业执照
+	public static final int AZZ_BUSINESS_IMAGE_TYPE = 2;
+	
+	//图像
+	public static final int AZZ_AVATAR_IMAGE_TYPE = 3;
+	
+	//其他图
+	public static final int AZZ_OTHER_IMAGE_TYPE = 4;
+	
+	//支持的图片格式
+	public static final String AZZ_IMAGE_SUFFIX = "bmp|jpg|png|git|webp|tiff";
+	
+	//图片最大值 MB
+	public static final int IMAGE_SIZE = 20;
 }
 
---
+++ azz-common-parent/azz-common-util/src/main/java/com/azz/util/AzzImageUtil.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ * Project Key : CPPII
+ * Create on 2018年10月23日 下午3:35:06
+ * Copyright (c) 2018. 爱智造.
+ * 注意：本内容仅限于爱智造内部传阅，禁止外泄以及用于其他的商业目的
+ ******************************************************************************/
+ 
+package com.azz.util;
+
+import com.azz.core.constants.FileConstants;
+
+/**
+ * <P>TODO</P>
+ * @version 1.0
+ * @author 刘建麟  2018年10月23日 下午3:35:06
+ */
+public class AzzImageUtil {
+	
+	/**
+	 * <p>根据平台类型获取平台名称</p>
+	 * @param plattype
+	 * @return
+	 * @author 刘建麟  2018年10月23日 下午3:38:27
+	 */
+	public static String getPlatByType(Integer plattype) {
+		if(null == plattype) {
+			return "";
+		}
+		if(FileConstants.AZZ_MERCHANT == plattype) {
+			return FileConstants.MERCHANT_IMAGE_PATH;
+		}
+		if(FileConstants.AZZ_CLIENT == plattype) {
+			return FileConstants.CLIENT_IMAGE_PATH;
+		}
+		if(FileConstants.AZZ_PLATFORM == plattype) {
+			return FileConstants.PLATFORM_IMAGE_PATH;
+		}
+		
+		return "";
+	}
+	
+	/**
+	 * <p>根据图片类型获取图片文件夹名称</p>
+	 * @param plattype
+	 * @return
+	 * @author 刘建麟  2018年10月23日 下午3:38:45
+	 */
+	public static String getImageByType(Integer imagetype) {
+		if(null == imagetype) {
+			return "";
+		}
+		if(FileConstants.AZZ_AVATAR_IMAGE_TYPE == imagetype) {
+			return FileConstants.AZZ_AVATAR_PATH;
+		}
+		if(FileConstants.AZZ_LEGAL_IMAGE_TYPE == imagetype) {
+			return FileConstants.AZZ_LEGAL_PERSON_PATH;
+		}
+		if(FileConstants.AZZ_BUSINESS_IMAGE_TYPE == imagetype) {
+			return FileConstants.AZZ_BUSINESS_LICENSE_PATH;
+		}
+		if(FileConstants.AZZ_OTHER_IMAGE_TYPE == imagetype) {
+			return FileConstants.AZZ_OTHER_PATH;
+		}
+		
+		return "";
+	}
+}	
+

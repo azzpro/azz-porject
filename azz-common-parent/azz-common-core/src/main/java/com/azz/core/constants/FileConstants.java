adbc1c2341196637496ad15e53a638e766ad71e5
jianlin <m18676389907@163.com>
2018-10-23 14:54:29 +0800

    常量类

---
+++ azz-common-parent/azz-common-core/src/main/java/com/azz/core/constants/FileConstants.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Project Key : CPPII
+ * Create on 2018年10月23日 下午2:35:31
+ * Copyright (c) 2018. 爱智造.
+ * 注意：本内容仅限于爱智造内部传阅，禁止外泄以及用于其他的商业目的
+ ******************************************************************************/
+ 
+package com.azz.core.constants;
+/**
+ * <P>TODO</P>
+ * @version 1.0
+ * @author 刘建麟  2018年10月23日 下午2:35:31
+ */
+public class FileConstants {
+	
+	public static final String AZZ_LEGAL_PERSON_PATH = "legal_person_pic";
+	
+	public static final String AZZ_BUSINESS_LICENSE_PATH = "business_license_pic";
+	
+	public static final int AZZ_IMAGE_TYPE = 1;
+	
+	public static final String AZZ_IMAGE_SUFFIX = "bmp|jpg|png";
+}
+

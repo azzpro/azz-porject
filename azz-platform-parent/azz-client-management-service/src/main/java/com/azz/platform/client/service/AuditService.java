3dc356b2da8968aece082a30cdbde4ddd30b259d
THINK <THINK@192.168.1.115>
2018-10-25 16:25:04 +0800

    客户审核

--- azz-platform-parent/azz-client-management-service/src/main/java/com/azz/platform/client/service/AuditService.java
+++ azz-platform-parent/azz-client-management-service/src/main/java/com/azz/platform/client/service/AuditService.java
@@ -5,6 +5,8 @@
 
 package com.azz.platform.client.service;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,10 +16,13 @@ import com.azz.core.common.JsonResult;
 import com.azz.core.common.errorcode.PlatformUserErrorCode;
 import com.azz.core.exception.BaseException;
 import com.azz.platform.client.common.constants.AuditConstants;
+import com.azz.platform.client.common.constants.ClientConstants;
 import com.azz.platform.client.mapper.ClientApplyMapper;
+import com.azz.platform.client.mapper.ClientUserCompanyMapper;
 import com.azz.platform.client.mapper.ClientUserMapper;
 import com.azz.platform.client.pojo.ClientApply;
 import com.azz.platform.client.pojo.ClientUser;
+import com.azz.platform.client.pojo.ClientUserCompany;
 import com.azz.platform.client.pojo.bo.AuditParam;
 import com.azz.util.JSR303ValidateUtils;
 import com.azz.util.ObjectUtils;
@@ -40,6 +45,9 @@ public class AuditService {
     @Autowired
     ClientUserMapper clientUserMapper;
 
+    @Autowired
+    ClientUserCompanyMapper clientUserCompanyMapper;
+    
     public JsonResult<String> auditClient(@RequestBody AuditParam param) {
         JSR303ValidateUtils.validate(param);
         int isExist = clientUserMapper.checkClientApplyInfo(param.getClientUserCode());
@@ -57,16 +65,41 @@ public class AuditService {
             throw new BaseException(PlatformUserErrorCode.PLATFORM_CLIENT_AUDIT_NO_EXIST);
         }
         
-        ClientApply ca = new ClientApply();
+        ClientApply clientApplyObj = clientApplyMapper.selectByClientUserIdStatus(clientUser.getId());
+        
         if (param.getStatus().equals(AuditConstants.AuditStatus.PASSED.getValue())) {
-            // 审核通过
+            // 审核通过(clientApply)
+            clientApplyObj.setStatus(AuditConstants.AuditStatus.PASSED.getValue());
             
+            // 更新客户为企业用户
+            clientUser.setStatus(ClientConstants.EnterpriseType.ENTERPRISE.getValue());
+            clientUserMapper.updateByPrimaryKeySelective(clientUser);
             
-            clientApplyMapper.updateByPrimaryKeySelective(ca);
+            // 申请基本信息和资质信息新增到公司表
+            ClientUserCompany cuc = new ClientUserCompany();
+            cuc.setClientUserCode(clientUser.getClientUserCode());
+            cuc.setCompanyName(clientApplyObj.getCompanyName());
+            cuc.setCreditCode(clientApplyObj.getCreditCode());
+            cuc.setCompanyTel(clientApplyObj.getCompanyTel());
+            cuc.setTradingCertificateFirstFileName(clientApplyObj.getTradingCertificateFirstFileName());
+            cuc.setTradingCertificateFirstFileUrl(clientApplyObj.getTradingCertificateFirstFileUrl());
+            cuc.setTradingCertificateSecondFileName(clientApplyObj.getTradingCertificateSecondFileUrl());
+            cuc.setTradingCertificateSecondFileUrl(clientApplyObj.getTradingCertificateSecondFileUrl());
+            cuc.setTradingCertificateThirdFileName(clientApplyObj.getTradingCertificateThirdFileName());
+            cuc.setTradingCertificateThirdFileUrl(clientApplyObj.getTradingCertificateThirdFileUrl());
+            cuc.setCreateTime(new Date());
+            clientUserCompanyMapper.insertSelective(cuc);
+            
         } else if (param.getStatus().equals(AuditConstants.AuditStatus.REFUSED.getValue())) {
             // 审核拒绝
+            clientApplyObj.setStatus(AuditConstants.AuditStatus.REFUSED.getValue());
         }
-        return null;
+        // 更新申请信息的审核人，审核时间
+        clientApplyObj.setAuditor(param.getAuditor());
+        clientApplyObj.setAuditorTime(new Date());
+        clientApplyMapper.updateByPrimaryKeySelective(clientApplyObj);
+        
+        return JsonResult.successJsonResult();
     }
 
 }

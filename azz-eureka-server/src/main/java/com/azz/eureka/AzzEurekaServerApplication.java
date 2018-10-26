7f1b5df64cd20f19df03ab8063e6c3741552057b
jianlin <m18676389907@163.com>
2018-10-26 20:35:50 +0800

    1

--- azz-eureka-server/src/main/java/com/azz/eureka/AzzEurekaServerApplication.java
+++ azz-eureka-server/src/main/java/com/azz/eureka/AzzEurekaServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class AzzEurekaServerApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.devtools.restart.enabled", "true");
 		SpringApplication.run(AzzEurekaServerApplication.class, args);
 	}
 }

0e4ccbfe95463c81a014cbb92fdcf5686242a411
jianlin <m18676389907@163.com>
2018-10-27 15:11:42 +0800

    1

--- azz-system-parent/azz-sequence-service/src/main/java/com/azz/AzzSequenceServiceApplication.java
+++ azz-system-parent/azz-sequence-service/src/main/java/com/azz/AzzSequenceServiceApplication.java
@@ -4,16 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 @EnableTransactionManagement
-@EnableHystrix
-@EnableHystrixDashboard
 public class AzzSequenceServiceApplication {
 
 	public static void main(String[] args) {

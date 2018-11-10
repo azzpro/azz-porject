19b650290fba39cea6686d81b2519a26491f3c90
THINK <THINK@192.168.1.118>
2018-11-10 18:12:32 +0800

    代码调整

--- azz-common-parent/azz-common-core/src/main/java/com/azz/core/common/config/WebConfig.java
+++ azz-common-parent/azz-common-core/src/main/java/com/azz/core/common/config/WebConfig.java
@@ -7,10 +7,21 @@
  
 package com.azz.core.common.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 /**
  * <P>web相关配置</P>
@@ -27,6 +38,24 @@ public class WebConfig extends WebMvcConfigurerAdapter {
                 .allowedMethods(CorsConfiguration.ALL)
                 .allowedOrigins(CorsConfiguration.ALL);
         super.addCorsMappings(registry);
+    }
+    
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters(){
+        //1.需要定义一个convert转换消息的对象;
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        //2:添加fastJson的配置信息;
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        //3处理中文乱码问题
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        //4.在convert中添加配置信息.
+        fastJsonHttpMessageConverter.setSupportedMediaTypes(fastMediaTypes);
+        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastJsonHttpMessageConverter;
+        return new HttpMessageConverters(converter);
+
     }
 }
 

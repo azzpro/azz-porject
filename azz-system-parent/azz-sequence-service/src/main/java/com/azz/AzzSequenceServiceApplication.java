5808e4c34a6d5bfa4d51c38cd32991d49bf7864a
gggggzaaa11@163.com <gggggzaaa11@163.com>
2018-11-12 11:08:41 +0800

    1

--- azz-system-parent/azz-sequence-service/src/main/java/com/azz/AzzSequenceServiceApplication.java
+++ azz-system-parent/azz-sequence-service/src/main/java/com/azz/AzzSequenceServiceApplication.java
@@ -14,6 +14,6 @@ public class AzzSequenceServiceApplication {
 
 	public static void main(String[] args) {
 		System.setProperty("spring.devtools.restart.enabled", "true");
-		 SpringApplication.run(AzzSequenceServiceApplication.class, args);
+		SpringApplication.run(AzzSequenceServiceApplication.class, args);
 	}
 }

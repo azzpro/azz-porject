af98d9ae0f6817e487f69fe6fc97b6ded63fba02
gggggzaaa11@163.com <gggggzaaa11@163.com>
2018-11-12 14:02:01 +0800

    1

--- azz-system-parent/azz-sequence-service/src/main/java/com/azz/AzzSequenceServiceApplication.java
+++ azz-system-parent/azz-sequence-service/src/main/java/com/azz/AzzSequenceServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication

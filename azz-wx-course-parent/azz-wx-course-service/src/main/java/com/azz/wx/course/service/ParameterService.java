9810531d9074154081b6e2a1229a469bd5322fae
THINK <THINK@192.168.1.121>
2019-01-05 13:20:53 +0800

    参数管理

---
+++ azz-wx-course-parent/azz-wx-course-service/src/main/java/com/azz/wx/course/service/ParameterService.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Project Key : CPPII
+ * Create on 2019年1月5日 上午10:17:42
+ * Copyright (c) 2018. 爱智造.
+ * 注意：本内容仅限于爱智造内部传阅，禁止外泄以及用于其他的商业目的
+ ******************************************************************************/
+ 
+package com.azz.wx.course.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * <P>TODO</P>
+ * @version 1.0
+ * @author 彭斌  2019年1月5日 上午10:17:42
+ */
+@Transactional(rollbackFor = Exception.class)
+@Service
+public class ParameterService {
+    
+}
+

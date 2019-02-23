63eba20ded79051c94bb707eec42d407d9982a79
m18676389907@163.com <m18676389907@163.com>
2019-02-23 11:42:23 +0800

    1

--- azz-client-parent/azz-client-web/src/main/java/com/azz/controller/QQLoginController.java
+++ azz-client-parent/azz-client-web/src/main/java/com/azz/controller/QQLoginController.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -50,6 +51,7 @@ import com.qq.connect.oauth.Oauth;
  */
 @RestController
 @RequestMapping("/azz/api/qq")
+@CrossOrigin
 public class QQLoginController {
 
 	private final Logger log = LoggerFactory.getLogger(getClass());

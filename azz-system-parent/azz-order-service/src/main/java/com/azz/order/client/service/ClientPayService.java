c89d574b1e0b5bc255092dd6de60c3fbf9a01aea
m18676389907@163.com <m18676389907@163.com>
2019-03-04 11:16:38 +0800

    1

--- azz-system-parent/azz-order-service/src/main/java/com/azz/order/client/service/ClientPayService.java
+++ azz-system-parent/azz-order-service/src/main/java/com/azz/order/client/service/ClientPayService.java
@@ -157,7 +157,7 @@ public class ClientPayService {
 		params.put("timeoutNotifyUrl", "");
 		Set<Entry<String, String>> entrySet = params.entrySet();
 		for (Entry<String, String> entry : entrySet) {
-			System.out.println("key--->:"+entry.getKey()+":::value--->:"+entry.getValue());
+			log.info("key--->:"+entry.getKey()+":::value--->:"+entry.getValue());
 		}
 		Map<String, String> result = new HashMap<>();
 		String uri = YeepayService.getUrl(YeepayService.TRADEORDER_URL);

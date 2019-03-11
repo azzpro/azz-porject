b404c61a3e1d114c16b85e458989da0beeb8527d
THINK <THINK@192.168.1.117>
2019-03-11 17:32:46 +0800

    代码调整

--- azz-crawler/src/main/java/com/azz/crawler/config/GanjiKeyWordData.java
+++ azz-crawler/src/main/java/com/azz/crawler/config/GanjiKeyWordData.java
@@ -22,6 +22,7 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.alibaba.fastjson.JSONObject;
 import com.azz.crawler.pojo.GanJiTitle;
 
 import lombok.Getter;
@@ -36,7 +37,7 @@ public class GanjiKeyWordData {
 
     private static final String URL = "http://sz.ganji.com/";
     
-    private static final String qqjeURL = "https://www.qyxxpd.com/searchPxSchool/";
+    private static final String GANJIINDEXURL = "http://www.ganji.com/index.htm";
     //private static final String schoolURL = "http://www.zkbedu.com/school";
     
     @Getter
@@ -52,46 +53,8 @@ public class GanjiKeyWordData {
             LOGGER.info("正在初始化赶集网数据...");
             Document doc = Jsoup.connect(URL).get();
             Elements allLies = doc.getElementsByClass("content-col");
-           for (Element eachLie : allLies) {
-                //Elements allBoxes1 = eachLie.getElementsByClass("col col-1");
-                //Elements allBoxes2 = eachLie.getElementsByClass("col col-2");
-                Elements allBoxes4 = eachLie.getElementsByClass("col col-4");
-                /*for (Element eachBox1 : allBoxes1) {
-                    List<GanJiTitle> subTitles = new ArrayList<>();
-                    String categoryArray[] = {"category house","category secondhand-car","category new-car","category car-service","category zhaoshang noborder"};
-                    for (int i = 0; i < categoryArray.length; i++) {
-                        String categoryStr = categoryArray[i];
-                        Elements categoryVal = eachBox1.getElementsByClass(categoryStr);
-                        for (Element eachBoxHouse : categoryVal) {   
-                            Elements elements = eachBoxHouse.select("a");
-                            for (Element element : elements) {
-                                String url = URL + element.attr("href");
-                                String name = element.select("a").html();
-                                subTitles.add(new GanJiTitle(name, url, null));
-                            }
-                        }
-                    }
-                    allTitles.add(new GanJiTitle("第一列数据分析", null, subTitles));
-                }
-                
-                for (Element eachBox2 : allBoxes2) {
-                    List<GanJiTitle> subTitles = new ArrayList<>();
-                    String categoryArray[] = {"category clearfix","category ershoushichang","category"};
-                    for (int i = 0; i < categoryArray.length; i++) {
-                        String categoryStr = categoryArray[i];
-                        Elements categoryVal = eachBox2.getElementsByClass(categoryStr);
-                        for (Element eachBoxHouse : categoryVal) {   
-                            Elements elements = eachBoxHouse.select("a");
-                            for (Element element : elements) {
-                                String url = URL + element.attr("href");
-                                String name = element.select("a").html();
-                                subTitles.add(new GanJiTitle(name, url, null));
-                            }
-                        }
-                    }
-                    allTitles.add(new GanJiTitle("第二列数据分析", null, subTitles));
-                }*/
-                
+           for (Element eachLie : allLies) {        
+                Elements allBoxes4 = eachLie.getElementsByClass("col col-4");               
                 for (Element eachBox4 : allBoxes4) {
                     List<GanJiTitle> subTitles = new ArrayList<>();
                     String categoryArray[] = {"category business"};
@@ -111,6 +74,11 @@ public class GanjiKeyWordData {
                 }
                 
             }
+           
+           // 初始化地区信息
+           
+           
+           
             LOGGER.info("初始化赶集网数据完毕...");
         } catch (IOException e) {
             LOGGER.error("初始化赶集网数据出错", e);
@@ -128,13 +96,29 @@ public class GanjiKeyWordData {
         //row0.createCell(1).setCellValue("联系方式");
         //row0.createCell(2).setCellValue("专业信息");
         try {
-            Document doc = Jsoup.connect(qqjeURL).get();
-            Elements allLies = doc.getElementById("search").children();
-            System.out.println("allLies.size()="+allLies.size());
+            Document doc = Jsoup.connect(GANJIINDEXURL).get();
+            Elements allLies = doc.select("div.all-city dl");
+            System.out.println("所有dd标签的信息="+allLies);
             for (int i = 0; i < allLies.size(); i++) {
+                Elements allCityLies = allLies.get(i).select("dd");
+                System.out.println("allCityLies："+allCityLies);
+                //Elements allCityDt = doc.select("div.all-city dd dt");
+                //System.out.println("allCityLies="+allCityLies);
+                
+                /*for (int j = 0; j < allCityLies.size(); j++) {
+                    Elements allCityDd = allLies.get(j).select("dd");
+                    for (int k = 0; k < allCityDd.size(); k++) {
+                        System.out.println(JSONObject.toJSONString(allCityDd.get(k).text().split(" ")));
+                    }
+                }*/
+                
+               /*for (int j = 0; j < allCityLies.size(); j++) {
+                    System.out.println("市区链接："+allLies.get(j).select("a").attr("href"));
+                    System.out.println("市区名字："+allLies.get(j).select("a").text());
+                }*/
                 //HSSFRow row = sheet.createRow(i);
-                String schoolName = allLies.get(i).select("dl.schoolinfo dd.course a h1").text();
-                System.out.println("schoolName="+schoolName);
+                //String schoolName = allLies.get(i).select("dl.schoolinfo dd.course a h1").text();
+                //System.out.println("schoolName="+schoolName);
                 //row.createCell(0).setCellValue(schoolName);
                 //String schoolNameUrl = allLies.get(i).getElementsByClass("keyword").select("a").attr("href");
                 //Document shoolDetaildoc = Jsoup.connect(schoolNameUrl).get();
@@ -147,7 +131,7 @@ public class GanjiKeyWordData {
                 //}
                 //row.createCell(1).setCellValue(str);
                 
-                System.out.println("================================================");
+                //System.out.println("市区链接："+allLies.get(i).select("a").attr("href"));
             }
             
             try {
---
+++ azz-crawler/src/main/java/com/azz/crawler/pojo/CityInfo.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Project Key : CPPII
+ * Create on 2019年3月11日 下午5:29:17
+ * Copyright (c) 2018. 爱智造.
+ * 注意：本内容仅限于爱智造内部传阅，禁止外泄以及用于其他的商业目的
+ ******************************************************************************/
+ 
+package com.azz.crawler.pojo;
+
+import lombok.Data;
+
+/**
+ * <P>TODO</P>
+ * @version 1.0
+ * @author 彭斌  2019年3月11日 下午5:29:17
+ */
+@Data
+public class CityInfo {
+    private String cityUrl;
+    private String cityName;
+}
+

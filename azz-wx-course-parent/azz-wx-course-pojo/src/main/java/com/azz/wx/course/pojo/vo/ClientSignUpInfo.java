6fa3e4b24d1a7827343689f4de3e136c26b16840
THINK <THINK@SZLX-20180112JJ>
2019-04-19 18:18:55 +0800

    1

---
+++ azz-wx-course-parent/azz-wx-course-pojo/src/main/java/com/azz/wx/course/pojo/vo/ClientSignUpInfo.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Project Key : CPPII
+ * Create on 2019年4月17日 下午7:16:07
+ * Copyright (c) 2018. 爱智造.
+ * 注意：本内容仅限于爱智造内部传阅，禁止外泄以及用于其他的商业目的
+ ******************************************************************************/
+ 
+package com.azz.wx.course.pojo.vo;
+
+import lombok.Data;
+
+/**
+ * <P>TODO</P>
+ * @version 1.0
+ * @author 黄智聪  2019年4月17日 下午7:16:07
+ */
+@Data
+public class ClientSignUpInfo {
+	
+    private String nickname;
+
+    private String headImageUrl;
+
+}
+

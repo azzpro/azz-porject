794082370ed9bd88f0cf0d0aa6654d6209bdee0e
THINK <THINK@SZLX-20180112JJ>
2019-05-10 11:03:06 +0800

    1

--- azz-common-parent/azz-common-util/src/main/java/com/azz/util/OkHttpUtil.java
+++ azz-common-parent/azz-common-util/src/main/java/com/azz/util/OkHttpUtil.java
@@ -3,6 +3,7 @@ package com.azz.util;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.collections.MapUtils;
@@ -17,6 +18,7 @@ import okhttp3.HttpUrl;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Request.Builder;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
@@ -110,6 +112,45 @@ public class OkHttpUtil {
             log.error("发送POST请求异常", e);
         }
         log.debug("formData发送POST请求结果:" + result);
+        return result;
+    }
+    
+    /**
+     * 模拟表单提交发送post请求，参数无顺序
+     *
+     * @param url 发送的URL
+     * @param params 发送表单的key-value，数据在requestbody formData中
+     * @return 返回的数据，字符串格式
+     * @author 王好(17720589316)
+     * @since 2016-9-23 11:56:47
+     */
+    public static String postFormData(String url, Map<String, String> params, Map<String,String> headers) {
+        // 构造表单对象
+//        log.debug("post请求URL[{}],请求参数:{}", url, JSON.toJSONString(params));
+        FormBody.Builder formBodyBuilder = new FormBody.Builder();
+        if (CollectionUtils.isNotEmpty(params)) {
+            for (Entry<String, String> entry : params.entrySet()) {
+                formBodyBuilder.add(entry.getKey(), entry.getValue());
+            }
+        }
+        FormBody formBody = formBodyBuilder.build();
+        Builder post = new Request.Builder().url(url).post(formBody);
+        if(headers != null && headers.size() > 0) {
+        	Set<String> keys = headers.keySet();
+        	for (String key : keys) {
+        		post.addHeader(key, headers.get(key));
+        	}
+        }
+        // 构造request对象
+        Request request = post.build();
+        String result = "";
+        try {
+            Response response = client.newCall(request).execute();
+            result = response.body().string();
+        } catch (IOException e) {
+            log.error("发送POST请求异常", e);
+        }
+//        log.debug("formData发送POST请求结果:" + result);
         return result;
     }
     
